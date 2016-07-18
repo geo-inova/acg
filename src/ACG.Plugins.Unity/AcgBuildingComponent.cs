9972bf5ff7d8e38c35c078dda9064fc063ee533e
Maksim Sestic <max@geoinova.com>
2016-07-18 18:07:52 +0200

    Added AcgBuildingComponent.cs

--- src/ACG.Plugins.Unity/AcgBuildingComponent.cs
+++ src/ACG.Plugins.Unity/AcgBuildingComponent.cs
@@ -4,22 +4,25 @@ using System.Text;
 
 using UnityEngine;
 
-using ACG.Core;
 using ACG.Core.Objects;
-using ACG.Core.Readers;
 
 namespace ACG.Plugins.Unity
 {
     /// <summary>
-    /// Template class for AreaCAD-GIS Unity behaviour script.
+    /// Represents <see cref="AcgBuilding"/> Unity component.
     /// </summary>
-    public class AcgUnityBehaviourScript : MonoBehaviour
+    public class AcgBuildingComponent : MonoBehaviour
     {
+
+        /// <summary>
+        /// Gets or sets component's AreaCAD-GIS object data.
+        /// </summary>
+        public AcgBuilding ObjectData;
 
         // Use this for initialization
         void Start()
         {
-
+        
         }
 
         // Update is called once per frame
---
+++ src/ACG.Plugins.Unity/AcgBuildingComponentEditor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using UnityEngine;
+using UnityEditor;
+
+using ACG.Core.Objects;
+
+namespace ACG.Plugins.Unity
+{
+    /// <summary>
+    /// Represents <see cref="AcgBuilding"/> Unity component editor.
+    /// </summary>
+    [CustomEditor(typeof(AcgBuildingComponent))]
+    public class AcgBuildingComponentEditor : Editor
+    {
+
+        AcgBuildingComponent _target;
+
+        void OnEnable()
+        {
+            _target = (AcgBuildingComponent)target;
+        }
+
+        /// <summary>
+        /// Implement this function to make a custom inspector.
+        /// </summary>
+        public override void OnInspectorGUI()
+        {
+            GUILayout.BeginVertical();
+
+            GUILayout.Label("Building", EditorStyles.boldLabel);
+            _target.ObjectData.HeightFixed = EditorGUILayout.DoubleField("Height", _target.ObjectData.HeightFixed);
+
+            GUILayout.EndVertical();
+
+            //If we changed the GUI aply the new values to the script
+            if (GUI.changed)
+            {
+                EditorUtility.SetDirty(_target);
+            }
+        }
+    }
+}
